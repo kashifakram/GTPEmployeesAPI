6f7c962cb750e1ca8cc6365409a4e89fa0714461
Kashif Akram <kashif@kashifakram.com>
2022-06-02 12:45:33 +1000

    chore: cleaning

--- EmployeesAPI/Employees.API/Controllers/EmployeesController.cs
+++ EmployeesAPI/Employees.API/Controllers/EmployeesController.cs
@@ -2,7 +2,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace Employees.API.Controllers
 {
